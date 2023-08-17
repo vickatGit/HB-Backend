b8ed3234f42ff39b883bad493e019bed878b4dc2
Vikas Gangishetty <gangishettyvikas@gmail.com>
2023-08-17 20:01:08 +0530

    group habit validation schema modified

---
+++ models/HabitModels/validationSchema/GroupHabitValidationSchema.js
@@ -0,0 +1,20 @@
+const Joi = require("joi");
+
+const GroupHabitValidationSchema = Joi.object({
+    _id : Joi.string.optional(),
+    title:Joi.string().required(),
+    description:Joi.string().required(),
+    reminderQuestion:Joi.string().optional(),
+    startDate:Joi.date().required(),
+    endDate:Joi.date().required().greater(Joi.ref("startDate")),
+    isReminderOn:Joi.boolean().required(),
+    reminderTime:Joi.date().when('isReminderOn',{   
+        is:true,
+        then:Joi.date().required(),
+        otherwise:Joi.date().optional()
+    }),
+    localId : Joi.string().required(),
+    admin : Joi.string().required,
+    members : Joi.array(Joi.string().required),
+    habits : Joi.array(Joi.string().required)
+})
