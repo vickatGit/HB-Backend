c119695258ff163838d27f8ea001fcd917f8cd9c
Vikas Gangishetty <gangishettyvikas@gmail.com>
2023-08-29 19:07:02 +0530

    follow following merged in get members api

--- Services/SocialService.js
+++ Services/SocialService.js
@@ -1,26 +1,29 @@
 const User = require("../models/AuthModels/AuthModel");
 const Follow = require("../models/SocialModels/FollowModel");
 
-
-const GetUser = async(userId) => {
+const GetUser = async (userId) => {
   try {
-    return await User.findOne({ _id:userId }, { password: 0 });
+    return await User.findOne({ _id: userId }, { password: 0 });
   } catch (error) {
     throw new Error(error);
   }
-}
+};
 const UpdateProfile = async (userId, user) => {
   try {
-    const res = await User.findOne({ _id: userId })
-    await User.updateOne({ _id: userId }, { $set: {
-      userBio:user.userBio,
-      userAvatar:user.userAvatar
-    } });
-    console.log(res)
+    const res = await User.findOne({ _id: userId });
+    await User.updateOne(
+      { _id: userId },
+      {
+        $set: {
+          userBio: user.userBio,
+          userAvatar: user.userAvatar,
+        },
+      }
+    );
+    console.log(res);
   } catch (error) {
     throw new Error(error);
   }
-  
 };
 const GetUsersByName = async (query) => {
   try {
@@ -31,9 +34,9 @@ const GetUsersByName = async (query) => {
 };
 const FollowUser = async (follows, to) => {
   try {
-    await Follow.create({follows: follows,to: to,});
-    await User.updateOne({_id:follows},{ $inc : {followings:1} })
-    await User.updateOne({_id:to},{ $inc : {followers:1} })
+    await Follow.create({ follows: follows, to: to });
+    await User.updateOne({ _id: follows }, { $inc: { followings: 1 } });
+    await User.updateOne({ _id: to }, { $inc: { followers: 1 } });
   } catch (error) {
     throw new Error(error);
   }
@@ -41,8 +44,8 @@ const FollowUser = async (follows, to) => {
 const UnfollowUser = async (follows, to) => {
   try {
     await Follow.deleteOne({ $and: [{ follows: follows }, { to: to }] });
-    await User.updateOne( {_id:follows} ,{ $inc : {followings:-1} })
-    await User.updateOne({_id:to},{ $inc : {followers:-1} })
+    await User.updateOne({ _id: follows }, { $inc: { followings: -1 } });
+    await User.updateOne({ _id: to }, { $inc: { followers: -1 } });
   } catch (error) {
     throw new Error(error);
   }
@@ -70,24 +73,31 @@ const GetFollowings = async (userId) => {
     throw new Error(error);
   }
 };
-const GetMembers =  async(userId) => {
+const GetMembers = async (userId) => {
   try {
-    return await Follow.find(
-      {$or : [{follows:userId},{to:userId}]},
-      {follows:0,_id:0}
-      ).populate({
-      path: "to",
-      select: ["-password"],
+    const users={}
+    let followings = await Follow.find({ follows: userId },{ follows: 0, _id: 0 })
+    .populate({ path: "to", select: ["-password"]  })
+    followings.forEach(element => {
+      const elem = {to:element.to}
+      users[element.to._id]=elem
     });
+    let followers = await Follow.find({ to: userId },{ to: 0, _id: 0 })
+    .populate({ path: "follows", select: ["-password"]})
+    followers.forEach(element => {
+      const elem = {to:element.follows}
+      users[element.follows._id]=elem
+    });
+    return  Object.values(users);
   } catch (error) {
-    throw new Error(error)
+    throw new Error(error);
   }
-}
+};
 const isUserFollowing = async (userId, friendId) => {
   try {
-    return await Follow.findOne({$and:[
-      { follows: userId }, { to: friendId }
-    ]});
+    return await Follow.findOne({
+      $and: [{ follows: userId }, { to: friendId }],
+    });
   } catch (error) {
     throw new Error(error);
   }
@@ -101,5 +111,5 @@ module.exports = {
   isUserFollowing,
   UpdateProfile,
   GetUser,
-  GetMembers
+  GetMembers,
 };
