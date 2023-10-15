ec147b596ec9ce7fbf4d596fc92e848a30519205
Vikas Gangishetty <gangishettyvikas@gmail.com>
2023-10-15 16:14:08 +0530

    absolute path

--- Services/UiService.js
+++ Services/UiService.js
@@ -1,8 +1,9 @@
 const fs = require('fs')
 const path = require('path')
+// require('D:/VS code/HB/Server/Ui')
 // const homeUiFilePath ="..\\Ui\\home_page.json"
 // const homeUiFilePath =`D:\\VS code\\HB\\Server\\Ui\\home_page.json`
-const homeUiFilePath = path.resolve('../Ui',"home_page.json")
+const homeUiFilePath = path.resolve('D:/VS code/HB/Server/Ui',"home_page.json")
 const GetUi = (user) => {
     try {
         console.log("uipath", homeUiFilePath)
