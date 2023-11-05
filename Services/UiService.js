116d9067160f88726ef1efef2276e2df340b96c5
Vikas Gangishetty <gangishettyvikas@gmail.com>
2023-11-05 14:51:32 +0530

    user avatar url binded in homme page json

--- Services/UiService.js
+++ Services/UiService.js
@@ -1,16 +1,19 @@
 const homeData = require('../Ui/home_page')
 const path = require('path')
+const {GetUserPictureUrl} = require('../Services/SocialService')
 // require('D:/VS code/HB/Server/Ui')
 // const homeUiFilePath ="..\\Ui\\home_page.json"
 // const homeUiFilePath =`D:\\VS code\\HB\\Server\\Ui\\home_page.json`
 // const homeUiFilePath = path.resolve('./')
-const GetUi = (user) => {
+const GetUi = async (user,userId) => {
     // console.log("paths",fs.readdirSync(homeUiFilePath))
     try {
         // console.log("uipath", homeUiFilePath)
         // let jsonData = fs.readFileSync(homeUiFilePath,'utf8')
+        const userAvatarUrl = await GetUserPictureUrl(userId)
         const homeUi = JSON.parse(homeData)
         homeUi.sections[1].elements[0].headerText = `Hi.. ${user.username}`
+        homeUi.sections[1].elements[1].url = userAvatarUrl
         return homeUi 
     } catch (error) {
         console.log("ui error ",error)
