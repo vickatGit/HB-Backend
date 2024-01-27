726519d5c01ad6122be8991680d72c020e3964ef
Vikas Gangishetty <gangishettyvikas@gmail.com>
2024-01-27 14:46:46 +0530

    add habit bug resolved

--- src/models/HabitModels/validationSchema/HabitValidationModel.js
+++ src/models/HabitModels/validationSchema/HabitValidationModel.js
@@ -1,24 +1,25 @@
-const Joi=require("joi");
-const EntriesValidationSchema = require('./EntriesValidationSchema')
+const Joi = require("joi");
+const EntriesValidationSchema = require("./EntriesValidationSchema");
+const EntryValidationSchema = require("./EntryValidationSchema");
 // const validator = (schema) => (payload) => schema.validate(payload,{abortEarly:false})
 
-const HabitValidationSchema=Joi.object({
-    _id:Joi.string().optional(),
-    title:Joi.string().required(),
-    description:Joi.string().required(),
-    reminderQuestion:Joi.string().optional(),
-    startDate:Joi.date().required(),
-    endDate:Joi.date().required().greater(Joi.ref("startDate")),
-    isReminderOn:Joi.boolean().required(),
-    reminderTime:Joi.date().when('isReminderOn',{   
-        is:true,
-        then:Joi.date().required(),
-        otherwise:Joi.date().optional()
-    }),
-    localId:Joi.string().required(),
-    entries:EntriesValidationSchema,
-    groupHabitId:Joi.string().optional()
-})
+const HabitValidationSchema = Joi.object({
+  _id: Joi.string().optional(),
+  title: Joi.string().required(),
+  description: Joi.string().required(),
+  reminderQuestion: Joi.string().optional(),
+  startDate: Joi.date().required(),
+  endDate: Joi.date().required().greater(Joi.ref("startDate")),
+  isReminderOn: Joi.boolean().required(),
+  reminderTime: Joi.date().when("isReminderOn", {
+    is: true,
+    then: Joi.date().required(),
+    otherwise: Joi.date().optional(),
+  }),
+  localId: Joi.string().required(),
+  entries: Joi.array().items(EntryValidationSchema).optional(),
+  groupHabitId: Joi.string().optional(),
+});
 
 // exports.habitValidator = validator(HabitValidationSchema)
- module.exports=HabitValidationSchema
+module.exports = HabitValidationSchema;
